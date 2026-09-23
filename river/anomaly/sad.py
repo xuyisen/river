1d1197f0f9c3ad27e98c5599b194123e9986f11b
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 16:40:31 -0400

    xuyisen__diff__id_37

--- river/anomaly/sad.py
+++ river/anomaly/sad.py
@@ -61,6 +61,7 @@ class StandardAbsoluteDeviation(anomaly.base.AnomalyDetector):
     def __init__(self, sub_stat: str = "mean"):
         self.variance = stats.Var()
         self.sub_stat = sub_stat
+        self.subtracted_statistic_estimator: stats.base.Univariate
 
         if self.sub_stat == "mean":
             self.subtracted_statistic_estimator = stats.Mean()
